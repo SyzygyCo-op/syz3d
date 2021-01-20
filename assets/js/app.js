c814f2ee382b3be024e703dcf680601679b258eb
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-01-20 07:26:49 -0800

    revert

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,8 @@ import "../css/app.scss";
 import "phoenix_html";
 import NProgress from "nprogress";
 import { handleMount } from "./game";
-import { configure } from "mobx";
-
-configure({
-  useProxies: "ifavailable",
-});
 
 NProgress.start();
-handleMount((fn) => {
+handleMount(() => {
   NProgress.done();
 });
