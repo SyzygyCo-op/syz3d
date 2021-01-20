26e3ac37da0bf500b50aa4cc269cd4b48a66375d
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-01-19 22:08:26 -0800

    Try ifavailable proxies mobx

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,11 @@ import "../css/app.scss";
 import "phoenix_html";
 import NProgress from "nprogress";
 import { handleMount } from "./game";
+import { configure } from "mobx";
+
+configure({
+  useProxies: "ifavailable",
+});
 
 NProgress.start();
 handleMount((fn) => {
