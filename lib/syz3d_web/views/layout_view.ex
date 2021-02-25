a61384a83296f3aa4a0c591e4af254d286581dc2
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-02-24 16:52:55 -0800

    a little cleanup on the backend

--- lib/syz3d_web/views/layout_view.ex
+++ lib/syz3d_web/views/layout_view.ex
@@ -1,14 +1,6 @@
 defmodule Syz3dWeb.LayoutView do
   use Syz3dWeb, :view
 
-def js_script_tag do
-  if Mix.env == :prod do
-    ~s(<script defer phx-track-static src="/js/app.js"></script>)
-  else
-    ~s(<script defer phx-track-static crossorigin src="http://localhost:8080/js/app.js"></script>)
-  end
-  end
-
   def css_link_tag do
     if Mix.env == :prod do
       ~s(<link rel="stylesheet" type="text/css" href="/css/app.css" media="screen,projection" />)
--- lib/syz3d_web/views/room/show.ex
+++ lib/syz3d_web/views/room/show.ex
@@ -3,4 +3,12 @@ defmodule Syz3dWeb.Room.ShowView do
   Route for joining a game room.
   """
   use Syz3dWeb, :view
+
+  def js_script_tag do
+    if Mix.env == :prod do
+      ~s(<script defer phx-track-static src="/js/app.js"></script>)
+    else
+      ~s(<script defer phx-track-static crossorigin src="http://localhost:8080/js/app.js"></script>)
+    end
+  end
 end
