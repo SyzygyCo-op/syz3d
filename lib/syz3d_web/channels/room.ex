9a6dd1fbe302853e34939fef4000b6a075106633
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-02-28 16:34:35 -0800

    server sends world_init, handle world_diff

--- lib/syz3d_web/channels/room.ex
+++ lib/syz3d_web/channels/room.ex
@@ -1,6 +1,5 @@
 defmodule Syz3dWeb.RoomChannel do
   use Phoenix.Channel
-  alias Syz3dWeb.Presence
 
   def join(_topic, _params, socket) do
     send(self(), :after_join)
@@ -8,18 +7,16 @@ defmodule Syz3dWeb.RoomChannel do
   end
 
   def handle_info(:after_join, socket) do
-    {:ok, _} = Presence.track(socket, socket.assigns.player_id, %{
-      online_at: inspect(System.system_time(:second)),
-      player_id: socket.assigns.player_id,
-      texture: socket.assigns.texture
-    })
+    body = %{
+      client_id: UUID.uuid4()
+    }
+    push(socket, "world_init", %{body: body})
 
-    push(socket, "presence_state", Presence.list(socket))
     {:noreply, socket}
   end
 
-  def handle_in("change_avatar", %{"body" => body}, socket) do
-    broadcast!(socket, "change_avatar", %{body: body})
+  def handle_in("world_diff", %{"body" => body}, socket) do
+    broadcast!(socket, "world_diff", %{body: body})
     {:noreply, socket}
   end
 end
