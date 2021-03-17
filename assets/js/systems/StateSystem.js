1a2d1e50c4d53fa1ad51d83bbb3cde9f9fdd4da3
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-03-16 17:26:18 -0700

    deduplicate

--- assets/js/systems/StateSystem.js
+++ assets/js/systems/StateSystem.js
@@ -1,17 +1,7 @@
-
 import * as DRMT from "dreamt";
 import * as MOBX from "mobx";
 import { R3FComponent } from "../components";
-
-export class ObservableState {
-  /**
-   * @type Set<DRMT.Entity>
-   */
-  entities = new Set();
-  constructor() {
-    MOBX.makeAutoObservable(this);
-  }
-}
+import { ObservableState } from "../state";
 
 export class StateSystem extends DRMT.System {
   static queries = {
