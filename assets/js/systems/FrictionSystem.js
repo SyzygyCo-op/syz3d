89d5ece5163e15f697370eac525e9fa3563aa0ec
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-04-20 19:17:58 -0700

    perf(frictionsystem)

--- assets/js/systems/FrictionSystem.js
+++ assets/js/systems/FrictionSystem.js
@@ -22,19 +22,20 @@ export class FrictionSystem extends DRMT.System {
     this.queries.entities.results.forEach((entity) => {
       const friction = entity.getComponent(FrictionComponent);
       if (entity.hasComponent(VelocityComponent)) {
+        /** @type Vector3 */
         const velocity = entity.getComponent(VelocityComponent).value;
-        velocity.addScaledVector(velocity, -1 * friction.linear);
+        if (velocity.x || velocity.y || velocity.z) {
+          velocity.addScaledVector(velocity, -1 * friction.linear);
+        }
       }
 
       if (entity.hasComponent(AngularVelocityComponent)) {
         /** @type Euler */
         const velocity = entity.getComponent(AngularVelocityComponent).value;
-        const damping = -1 * friction.angular;
-        velocity.x += velocity.x * damping;
-        velocity.y += velocity.y * damping;
-        velocity.z += velocity.z * damping;
+        if (velocity.x || velocity.y || velocity.z) {
+          Vector3.prototype.addScaledVector.call(velocity, velocity, -1 * friction.angular)
+        }
       }
     });
   }
 }
-
