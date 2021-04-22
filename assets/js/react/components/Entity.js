e7c77bbdbe075aef6e6ca1b8d0662f34339ea2e5
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-04-22 16:36:56 -0700

    perf/refactor: entity react components

--- assets/js/react/components/Entity.js
+++ assets/js/react/components/Entity.js
@@ -10,11 +10,9 @@ import {
   PositionComponent,
   RotationComponent,
 } from "../../components";
-import { Html } from "@react-three/drei";
 import { gameLoop } from "../../world";
 import { USE_TWEENING } from "../../config";
-import { makeCopier } from "../../utils";
-import {  Object3D, Vector3 } from "three";
+import {EntityRender} from "./EntityRender";
 
 const stateComponentMap = {
   label: UILabelComponent,
@@ -64,37 +62,3 @@ export const Entity = ({ entity, showNameTags }) => {
   );
 };
 
-/**
- * @type React.ComponentType<{position: Vector3, rotation: Vector3, scale:
- *   Vector3, label: string, object3d: Object3D, boundingBox: Vector3} &
- *   import("../../state").ISettings>
- */
-export const EntityRender = ({
-  position,
-  rotation,
-  scale,
-  label,
-  object3d,
-  boundingBox,
-  showNameTags,
-}) => {
-  const groupRef = React.useRef(null);
-  /** TODO called when off screen? */
-  gameLoop.useTick(makeCopier(groupRef.current, "position", position));
-  gameLoop.useTick(makeCopier(groupRef.current, "rotation", rotation));
-  gameLoop.useTick(makeCopier(groupRef.current, "scale", scale));
-
-  // TODO add user setting for whether to show labels
-
-  return (
-    <group ref={groupRef} castShadow receiveShadow>
-      {label && showNameTags && (
-        // TODO use distanceFactor prop
-        <Html position-y={boundingBox && boundingBox.y} center>
-          <address>{label}</address>
-        </Html>
-      )}
-      {object3d && <primitive object={object3d} />}
-    </group>
-  );
-};
---
+++ assets/js/react/components/EntityRender.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { gameLoop } from "../../world";
+import { makeCopier } from "../../utils";
+import { Object3D, Vector3 } from "three";
+import { Html } from "@react-three/drei";
+/**
+ * @type React.ComponentType<{position: Vector3, rotation: Vector3, scale:
+ *   Vector3, label: string, object3d: Object3D, boundingBox: Vector3} &
+ *   import("../../state").ISettings>
+ */
+export const EntityRender = ({
+  position,
+  rotation,
+  scale,
+  label,
+  object3d,
+  boundingBox,
+  showNameTags,
+}) => {
+  const groupRef = React.useRef(null);
+  /** TODO called when off screen? */
+  gameLoop.useTick(makeCopier(groupRef.current, "position", position));
+  gameLoop.useTick(makeCopier(groupRef.current, "rotation", rotation));
+  gameLoop.useTick(makeCopier(groupRef.current, "scale", scale));
+
+  return (
+    <group ref={groupRef} castShadow receiveShadow>
+      {label && showNameTags && (
+        // TODO use distanceFactor prop
+        <Html position-y={boundingBox && boundingBox.y} center>
+          <address>{label}</address>
+        </Html>
+      )}
+      {object3d && <primitive object={object3d} />}
+    </group>
+  );
+};
