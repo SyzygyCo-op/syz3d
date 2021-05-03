02206e388620fa928689392c739dea79bdccd486
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-05-02 18:21:39 -0700

    remove code for rendering collision objects

--- assets/js/react/components/StationaryObject3DEntity.js
+++ assets/js/react/components/StationaryObject3DEntity.js
@@ -1,13 +1,11 @@
 
 import * as DRMT from "dreamt";
 import * as React from "react";
-import {Object3D, Mesh, Material} from "three";
 import {
   Object3DComponent,
   ScaleComponent,
   PositionComponent,
   RotationComponent,
-  UseGlftForCollisionTag,
 } from "../../components";
 
 const stateComponentMap = {
@@ -28,24 +26,8 @@ export const StationaryObject3DEntity = ({ entity }) => {
     sync,
   ] = DRMT.useStateFromComponentMap(entity, stateComponentMap);
 
-  const isForCollision = React.useMemo(()=>entity.hasComponent(UseGlftForCollisionTag), [entity])
-
   React.useEffect(sync, [entity]);
 
-  return object3d && <primitive object={isForCollision ? setCollisionMaterial(object3d) : object3d} position={position} rotation={rotation} scale={scale}/>
+  return object3d && <primitive object={object3d} position={position} rotation={rotation} scale={scale}/>
 };
 
-/** @param {Object3D} object3d */
-function setCollisionMaterial(object3d) {
-  object3d.traverse((child) => {
-    if(child.type === "Mesh") {
-      const mesh = /** @type Mesh */(child);
-      const material =/** @type Material */(/** @type any */(mesh.material).length ? mesh.material[0] : mesh.material);
-
-      material.transparent = true;
-      material.opacity = 0.5;
-    }
-  })
-
-  return object3d
-}
