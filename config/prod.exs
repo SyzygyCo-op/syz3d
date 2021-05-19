c6fc90892812931a17bdd7910b5afa1c33c0b79f
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-05-18 20:47:34 -0700

    fix: force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -13,6 +13,7 @@ config :syz3d, Syz3dWeb.Endpoint,
   url: [host: "syz3d.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: ["https://syzthrydy.com/", "https://syz3d.gigalixirapp.com/"]
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
