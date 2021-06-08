683b6639799d3560be0c6b22b0f9b13d6f08f3c5
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-06-08 09:45:44 -0700

    refactor(powerupmachine): remove bitmask

--- assets/js/state/PowerUpMachine.js
+++ assets/js/state/PowerUpMachine.js
@@ -1,9 +1,7 @@
-export const BIT_KEYBOARD = 1;
-export const BIT_VIRTUAL_GAMEPAD = 2;
-
 export class PowerUpMachine {
   static STATE_INIT = 0;
   static STATE_STARTED = 1;
+  static STATE_FINISHED = 2;
 
   /** @param {number} maxPowerUps
     */
@@ -11,45 +9,37 @@ export class PowerUpMachine {
     this.maxPowerUpsSqrt = Math.sqrt(maxPowerUps);
     this.powerUps = 0;
     this.state = PowerUpMachine.STATE_INIT;
-    this.activeBits = 0;
   }
 
   get finished() {
-    return this.state !== PowerUpMachine.STATE_INIT && this.activeBits == 0;
+    return this.state === PowerUpMachine.STATE_FINISHED || this.result === 1;
   }
 
   get started() {
-    return this.state !== PowerUpMachine.STATE_INIT && this.activeBits > 0;
+    return this.state === PowerUpMachine.STATE_STARTED;
   }
 
   get result() {
     return Math.sqrt(this.powerUps) / this.maxPowerUpsSqrt;
   }
 
-  /** @param {BIT_VIRTUAL_GAMEPAD | BIT_KEYBOARD} bit */
-  sendStart(bit) {
+  sendStart() {
     this.state = PowerUpMachine.STATE_STARTED;
-    this.activeBits |= bit;
   }
 
+  // TODO should be no-op if not in STATE_STARTED?
   sendTick() {
-    if(this.activeBits > 0) {
+    if(this.result < 1) {
       this.powerUps++;
     }
-
-    if(this.result === 1) {
-      this.activeBits = 0;
-    }
   }
 
-  /** @param {BIT_VIRTUAL_GAMEPAD | BIT_KEYBOARD} bit */
-  sendFinish(bit) {
-    this.activeBits &= ~bit;
+  sendFinish() {
+    this.state = PowerUpMachine.STATE_FINISHED;
   }
 
   reset() {
     this.powerUps = 0;
     this.state = PowerUpMachine.STATE_INIT;
-    this.activeBits = 0;
   }
 }
