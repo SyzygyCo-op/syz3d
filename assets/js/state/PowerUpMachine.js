0eb86f2c08c7040341582e2d1a2c1beb495c8e7c
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-06-08 11:30:41 -0700

    refactor(powerupmachine): impl touchend path

--- assets/js/state/PowerUpMachine.js
+++ assets/js/state/PowerUpMachine.js
@@ -1,10 +1,11 @@
 export class PowerUpMachine {
-  static STATE_INIT = 0;
-  static STATE_KEY_DOWN = 1;
-  static STATE_OK = 2;
+  static STATE_INIT = -1;
+  static STATE_OK = 0;
+  static STATE_CANCEL = 1;
+  static STATE_KEY_DOWN = 2;
+  static STATE_TOUCH_END = 3;
 
-  /** @param {number} maxPowerUps
-    */
+  /** @param {number} maxPowerUps */
   constructor(maxPowerUps) {
     this.maxPowerUpsSqrt = Math.sqrt(maxPowerUps);
     this.powerUps = 0;
@@ -12,32 +13,62 @@ export class PowerUpMachine {
   }
 
   get finished() {
-    return this.state === PowerUpMachine.STATE_OK;
+    return (
+      this.state === PowerUpMachine.STATE_OK ||
+      this.state === PowerUpMachine.STATE_CANCEL
+    );
   }
 
   get started() {
-    return this.state === PowerUpMachine.STATE_KEY_DOWN;
+    return (
+      this.state === PowerUpMachine.STATE_KEY_DOWN ||
+      this.state === PowerUpMachine.STATE_TOUCH_END
+    );
   }
 
   get result() {
     return Math.sqrt(this.powerUps) / this.maxPowerUpsSqrt;
   }
 
+  get resultOk() {
+    return this.state === PowerUpMachine.STATE_OK;
+  }
+
   sendKeyDown() {
     this.state = PowerUpMachine.STATE_KEY_DOWN;
   }
 
+  sendTouchEnd() {
+    this.state = PowerUpMachine.STATE_TOUCH_END;
+  }
+
   sendTick() {
-    if(this.result < 1 && this.state === PowerUpMachine.STATE_KEY_DOWN) {
-      this.powerUps++;
-      if(this.result === 1) {
-        this.state = PowerUpMachine.STATE_OK;
+    if (this.started) {
+      if (this.state === PowerUpMachine.STATE_KEY_DOWN) {
+        if (this.result < 1) {
+          this.powerUps++;
+        }
+        if (this.result === 1) {
+          this.state = PowerUpMachine.STATE_OK;
+        }
+      }
+      if (this.state === PowerUpMachine.STATE_TOUCH_END) {
+        this.powerUps++;
+        if (this.result > 1) {
+          this.state = PowerUpMachine.STATE_CANCEL;
+        }
       }
     }
   }
 
   sendKeyUp() {
     this.state = PowerUpMachine.STATE_OK;
+  }
+
+  sendTouchStart() {
+    if (this.result <= 1) {
+      this.state = PowerUpMachine.STATE_OK;
+    }
   }
 
   reset() {
--- assets/js/state/PowerUpMachine.test.js
+++ assets/js/state/PowerUpMachine.test.js
@@ -38,13 +38,13 @@ describe("PowerUpMachine: keydown path", () => {
     sut.sendKeyDown();
     sut.sendKeyUp();
     expect(sut.finished).toBe(true)
-    expect(sut.state).toBe(PowerUpMachine.STATE_OK)
+    expect(sut.resultOk).toBe(true)
   });
   test("is finished and in OK state after result reaches maxPowerUps", () => {
     sut.sendKeyDown();
     new Array(maxPowerUps).fill(0).forEach(() => sut.sendTick())
     expect(sut.finished).toBe(true)
-    expect(sut.state).toBe(PowerUpMachine.STATE_OK)
+    expect(sut.resultOk).toBe(true)
   });
   test("can be reset", () => {
     sut.sendKeyDown();
@@ -56,3 +56,55 @@ describe("PowerUpMachine: keydown path", () => {
   });
 });
 
+describe("PowerUpMachine: touchend path", () => {
+  /** @type PowerUpMachine */
+  let sut;
+  const maxPowerUps = 4;
+  beforeEach(() => {
+    sut = new PowerUpMachine(maxPowerUps);
+  });
+  test("result correlates to number of tick messages between a touchend message and a touchstart message", () => {
+    const result0 = sut.result;
+    sut.sendTouchEnd();
+    sut.sendTick();
+    const result1 = sut.result;
+    sut.sendTick();
+    const result2 = sut.result;
+    sut.sendTouchStart();
+    sut.sendTick();
+    expect(result1 > result0).toBe(true)
+    expect(result2 > result1).toBe(true)
+  });
+  test("ticks before touchend do not count", () => {
+    sut.sendTick();
+    sut.sendTouchEnd();
+    expect(sut.result).toBe(0);
+  });
+  test("ticks after touchstart do not count", () => {
+    sut.sendKeyDown();
+    sut.sendTouchStart();
+    sut.sendTick();
+    expect(sut.result).toBe(0);
+  });
+  test("is finished and in OK state after recieve touchstart if result <= maxPowerUps", () => {
+    sut.sendTouchEnd();
+    new Array(maxPowerUps).fill(0).forEach(() => sut.sendTick())
+    sut.sendTouchStart();
+    expect(sut.finished).toBe(true)
+    expect(sut.resultOk).toBe(true)
+  });
+  test("is finished and in non-OK state when result exceeds maxPowerUps", () => {
+    sut.sendTouchEnd();
+    new Array(maxPowerUps + 1).fill(0).forEach(() => sut.sendTick())
+    expect(sut.finished).toBe(true)
+    expect(sut.resultOk).toBe(false)
+  });
+  test("can be reset", () => {
+    sut.sendKeyDown();
+    sut.sendTick();
+    sut.sendKeyUp();
+    sut.reset();
+    expect(sut.result).toBe(0)
+    expect(sut.finished).toBe(false)
+  });
+});
