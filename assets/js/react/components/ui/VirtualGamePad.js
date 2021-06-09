7e2e233a83a9b541bb1b279695de58c2edb135a4
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-06-09 16:47:37 -0700

    feat(virtualgamepad): replace move joystick with forward/backward buttons

--- assets/js/react/components/ui/VirtualGamePad.js
+++ assets/js/react/components/ui/VirtualGamePad.js
@@ -6,24 +6,66 @@ import { localPlayer } from "../../../localPlayer";
 import { addStrafeVelocity, TurnCommand } from "../../../commands";
 import { PlayerInternalsComponent } from "../../../components";
 import { jumpStateMachine } from "../../../state";
+import { gameLoop } from "../../../world";
+import { UpOutlined, DownOutlined } from "@ant-design/icons";
 
 export const VirtualGamePad = () => {
+  const [moveFactor, setMoveFactor] = React.useState(0);
+
+  gameLoop.useTick(() => {
+    moveFactor && addStrafeVelocity(localPlayer, 0, moveFactor);
+  });
   return (
     <>
       <SideBar zIndex={zIndexes.virtualGamePad} side="left">
-        <JoyStick onMove={handleLook} label="look/jump"
+        <JoyStick
+          onMove={handleLook}
+          label="turn/jump"
           onTouchEnd={handleJumpTouchEnd}
           onTouchStart={handleJumpTouchStart}
         />
       </SideBar>
       <SideBar zIndex={zIndexes.virtualGamePad} side="right">
-        <JoyStick
-          label="move"
-          onMove={handleMove}
-        />
+        <button
+          style={{
+            height: "10vw",
+            backgroundColor: "rgba(255, 255, 255, 0.2)",
+            textShadow: "1px 1px 1px rgba(0, 0, 0, 0.5)",
+            textTransform: "uppercase",
+          }}
+          onTouchStart={handleStartMoveForward}
+          onTouchEnd={handleEndMoveForward}
+        >
+          <UpOutlined/>
+        </button>
+        <button
+          style={{
+            height: "10vw",
+            backgroundColor: "rgba(255, 255, 255, 0.2)",
+            textShadow: "1px 1px 1px rgba(0, 0, 0, 0.5)",
+            textTransform: "uppercase",
+          }}
+          onTouchStart={handleStartMoveBackward}
+          onTouchEnd={handleEndMoveBackward}
+        >
+          <DownOutlined/>
+        </button>
       </SideBar>
     </>
   );
+
+  function handleStartMoveForward() {
+    setMoveFactor(1);
+  }
+  function handleEndMoveForward() {
+    setMoveFactor(0);
+  }
+  function handleStartMoveBackward() {
+    setMoveFactor(-1);
+  }
+  function handleEndMoveBackward() {
+    setMoveFactor(0);
+  }
 
   /** @param {JoyStickMoveEvent} evt */
   function handleLook(evt) {
@@ -34,17 +76,9 @@ export const VirtualGamePad = () => {
     );
   }
 
-  /** @param {JoyStickMoveEvent} evt */
-  function handleMove(evt) {
-    addStrafeVelocity(
-      localPlayer,
-      Math.PI / 2 - evt.angle,
-      -Math.hypot(evt.xDistance, evt.yDistance)
-    );
-  }
-
   function handleJumpTouchEnd() {
     // TODO maybe physics system should tell the state machine when player is able to jump?
+    // TODO also maybe isTouchingStableSurface should be part of the top-level state?
     if (
       localPlayer.getComponent(PlayerInternalsComponent).isTouchingStableSurface
     ) {
