c5facc69fd28c26b4e13058e571248077c84089e
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-06-09 17:20:11 -0700

    fix: add misc PWA stuff

--- lib/syz3d_web/endpoint.ex
+++ lib/syz3d_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule Syz3dWeb.Endpoint do
     at: "/",
     from: :syz3d,
     gzip: false,
-    only: ~w(css fonts images js 3d favicon.ico robots.txt)
+    only: ~w(css fonts images js 3d favicon.ico robots.txt manifest.json)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
